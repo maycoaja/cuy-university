535e0283c268c6238a2113ff3ac3552ef231c1e9
maycoaja <maycohanaf@gmail.com>
2024-02-25 19:24:19 +0700

    add confirm and cuy emot

--- Guess-Cuy-Game.py
+++ Guess-Cuy-Game.py
@@ -8,19 +8,28 @@ print(f"={welcome_message}=")
 print("=====================")
 
 username = input("Please enter your name: ")
+
+cave_shape = "|_|"
+
+cave_empty = [cave_shape] * 4
+
+cave_cuy = cave_empty.copy()
+cave_cuy[cuy_position - 1] = "|˙Ⱉ˙|"
+
+cave_empty = ' '.join(cave_empty)
+cave_cuy = ' '.join(cave_cuy)
+
 print(f'''
 Hello {username}!!! See the cave below
-|_|  |_|  |_|  |_|
+{cave_empty}
 ''')
 
-while True:
-    user_answer = int(input("Guess where cuy is! [1/2/3/4]: "))
-
+user_answer = int(input("Guess where cuy is! [1/2/3/4]: "))
+verification = input("Are you sure for the answer? [y/n]")
+if verification.lower() == "y":
     if cuy_position == user_answer:
-        print("YOU WIN!")
-        break
+        print(f"You're right, I'm here \n{cave_cuy}")
     else:
-        print(f"You wrong! Cuy is in Cave {cuy_position}")
-        try_again = input("Try again? [y/n]: ")
-        if try_again.lower() == "n":
-            break
+        print(f"You're wrong, I'm here \n{cave_cuy}")
+else:
+    exit()
